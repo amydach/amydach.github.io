a193dc1780f2afba8f2c290bfb5443b7f5d10e20
Amy Dach <adach@live.com>
2019-10-04 13:08:13 -0500

    my third commit

--- app.py
+++ app.py
@@ -45,45 +45,26 @@ def welcome():
 
     )
 
-
-@app.route("/api/v1.0/names")
-def names():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
-
-    session.close()
-
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-
-    return jsonify(all_names)
-
-
 @app.route("/api/v1.0/precipitation")
 def precipation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    """Return a list of precipitation data including the date, prcp"""
+    # Query ??
+    results = session.query(df.date, df.prcp).all()
 
     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+    # Create a dictionary from the row data and append to a list of all_precipitation
+    all_prcp = []
+    for date, prcp in results:
+        precipitation_dict = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["prcp"] = prcp
+        all_precipitation.append(precipitation_dict)
 
-    return jsonify(all_passengers)
+    return jsonify(all_precipitation)
 
 
 if __name__ == '__main__':
